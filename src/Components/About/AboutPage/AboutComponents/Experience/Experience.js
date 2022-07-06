e80f4c4644996753cc5412b8e3f78eb03f836d4c
isaiasmella <isaias.mella2013@gmail.com>
2022-07-06 17:52:16 -0300

    casi terminado el responsive

--- src/Components/About/AboutPage/AboutComponents/Experience/Experience.js
+++ src/Components/About/AboutPage/AboutComponents/Experience/Experience.js
@@ -19,14 +19,16 @@ const Experience = () => {
             ease:'linear',
             duration:1,
             delay:.5
-        }}}><a className='place' href='https://www.grupoaoniken.com.ar/' target='_blank' rel="noreferrer">Aoniken</a> - Consultor DataBase</motion.p>
+        }}}><a className='place' href='https://www.grupoaoniken.com.ar/' target='_blank' rel="noreferrer">Aoniken S.R.L</a> - Database Consultant</motion.p>
         <motion.p 
         initial={{opacity:0}}
         animate={{opacity:1, transition:{
             ease:'linear',
             duration:1,
             delay:1.2
-        }}} className='experience-text'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint accusantium tenetur mollitia commodi fugiat at est earum dignissimos nesciunt, asperiores excepturi, harum repellendus nisi dolorem nihil, culpa dolores quis nobis exercitationem non error id modi corrupti? Ab quidem incidunt voluptas nihil ratione iure pariatur</motion.p>
+        }}} className='experience-text'>My job was to make queries in the database for the facilitation of customer's decision making and/or to edit the databases for any administrative errors or bad loading by the clients.
+        and/or edit the databases for any clerical errors or employee misloading that may have occurred.
+        (I was using SQLServer).</motion.p>
       </div>
       <div className='experience'>
         <motion.p 
@@ -36,7 +38,7 @@ const Experience = () => {
             duration:1,
             delay:1
         }}}
-        className='experience-place'><a className='place' href='https://www.bernardolew.com.ar/' target='_blank' rel="noreferrer">Bernardo Lew</a> - Frontend Developer</motion.p>
+        className='experience-place'><a className='place' href='https://www.bernardolew.com.ar/' target='_blank' rel="noreferrer">Bernardo Lew e Hijos S.R.L</a> - Frontend Developer</motion.p>
         <motion.p 
         initial={{opacity:0}}
         animate={{opacity:1, transition:{
@@ -44,7 +46,9 @@ const Experience = () => {
             duration:1.5,
             delay:1.6
         }}}
-        className='experience-text'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint accusantium tenetur mollitia commodi fugiat at est earum dignissimos nesciunt, asperiores excepturi, harum repellendus nisi dolorem nihil, culpa dolores quis nobis exercitationem non error id modi corrupti? Ab quidem incidunt voluptas nihil ratione iure pariatur</motion.p>
+        className='experience-text'>My task is to develop different programs that facilitate the use, control or management of tasks in the company.
+        These projects are developed in JavaScript with the React library for the frontend, HTML5, CSS3 and
+        Electron for the development of desktop applications.</motion.p>
       </div>
     </div>
   )
